7e3acd0b9dc138ce6af791e348a717774dfbc877
Gonzalo Cao Cabeza de Vaca <gonzalo.cao@gmail.com>
2017-05-16 16:14:39 +0200

    Basico function. Encode and print on screen if verbose

--- cryptoradio.py
+++ cryptoradio.py
@@ -1,44 +1,79 @@
 #!/usr/bin/python
 
 #IMPORT
-import sys, getopt
+import sys, argparse, ConfigParser
 from cryptography.fernet import Fernet
 
-def main(argv):
-   #CONSTS
-   key="UdU9JhVqFz2QtygmODnUM8XU56WV3TBq3i-QsGy1dR4="
-   inputfile="input.txt"
-   blocksize=47
-   offset=0
-   screenflag=0
+   #
+   # #CONSTS
+   # key="UdU9JhVqFz2QtygmODnUM8XU56WV3TBq3i-QsGy1dR4="
+   # inputfile="input.txt"
+   # blocksize=47
+   # offset=0
+   # screenflag=0
+   #
+   #
+   # #start encrypt
+   # cryptomachine = Fernet(key)
+   # with open(inputfile, 'r') as file:
+   #    file.seek(blocksize*offset,0)
+   #    while True:
+   #       token = file.read(blocksize)
+   #       if not token: break
+   #       encToken = cryptomachine.encrypt(token)
+   #       if screenflag:
+   #          print encToken
+   # file.close()
 
-   try:
-      opts, args = getopt.getopt(argv,"hsi:k:",["ifile=","key="])
-   except getopt.GetoptError:
-      print 'cryptoradio.py -i <inputfile>'
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print 'cryptoradio.py [-k encryption_key] [-s] -i <inputfile>'
-         sys.exit()
-      elif opt in ("-i", "--ifile"):
-         inputfile = arg
-      elif opt in ("-k", "--key"):
-         key = arg
-      elif opt in ("-s", "--screen"):
-         screenflag = 1
+def TestConfig(config):
+    try:
+        option = config.get("Encryption","key")
+        option = config.get("Encryption","input")
 
-   #start encrypt
-   cryptomachine = Fernet(key)
-   with open(inputfile, 'r') as file:
-      file.seek(blocksize*offset,0)
-      while True:
-         token = file.read(blocksize)
-         if not token: break
-         encToken = cryptomachine.encrypt(token)
-         if screenflag:
-            print encToken
-   file.close()
+    except:
+        print 'Error in config file'
+
+def RunEncryption(config, args):
+
+    key = config.get("Encryption","key")
+    inputfile = config.get("Encryption","input")
+    blocksize = config.getint("Encryption","blocksize")
+    offset = config.getint("Encryption","offset")
+
+    cryptomachine = Fernet(key)
+
+    with open(inputfile, 'r') as file:
+        file.seek(blocksize*offset,0)
+        while True:
+            token = file.read(blocksize)
+            if not token: break
+            encToken = cryptomachine.encrypt(token)
+            if args.verbose:
+                print encToken
+
+def RunDecryption(config):
+    print "TODO"
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    #parse arguments
+    parser = argparse.ArgumentParser(description='Crytoradio script.')
+    parser.add_argument('config_file', help='Config file')
+    parser.add_argument('-v',"--verbose", help="increase output verbosity",
+                        action="store_true")
+
+    args = parser.parse_args()
+
+    config = ConfigParser.ConfigParser()
+    if config.read(args.config_file):
+        TestConfig(config);
+        if config.get("Encryption","mode") == "encrypt":
+            RunEncryption(config,args)
+        elif config.get("Encryption","mode") == "decrypt":
+            RunDecryption(config,args)
+        else:
+            print "Unexpected mode of operation. Check config file"
+            exit(1)
+
+    else:
+        print 'Config file "' + args.config_file + '" not found';
+        exit(1);
